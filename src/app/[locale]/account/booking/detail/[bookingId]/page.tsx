e97a45912f6bfa659ee67632d0c1785836e08977
HanThamarat <98406282+HanThamarat@users.noreply.github.com>
2026-01-01 16:16:31 +0700

    Fetch api using redux.

--- src/app/[locale]/account/booking/detail/[bookingId]/page.tsx
+++ src/app/[locale]/account/booking/detail/[bookingId]/page.tsx
@@ -1,21 +1,46 @@
 "use client"
 
 import { useParams, useRouter } from "next/navigation"
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
+import { useAppDispatch } from "@/app/hooks/appDispatch";
+import { useSelector } from "react-redux";
+import { bookingtSelector } from "@/app/store/slice/bookingSlice";
+import { useSession } from "next-auth/react";
+import { getBookingDetail } from "@/app/store/slice/bookingSlice";
 
 export default function BookingDetailPage() {
 
     const { bookingId } = useParams();
+    const dispatch = useAppDispatch();
+    const { booking_detail } = useSelector(bookingtSelector);
     const router = useRouter();
+    const isFetching = useRef(false);
+    const { data: session } = useSession();
 
     useEffect(() => {
 
         if(!bookingId) return router.back();
 
-        console.log(bookingId);
-        
+        if (!session) return;
 
-    }, [bookingId]);
+        const fetchData = async () => {
+            if (isFetching.current) return;
+            const data = {
+                bookingId: bookingId as string,
+                accessToken: session?.authToken as string,
+            }
+            isFetching.current = true;
+            await dispatch(getBookingDetail(data));
+            isFetching.current = false;
+        }
+
+        fetchData();
+
+    }, [bookingId, session]);
+
+    useEffect(() => {
+        console.log(booking_detail);
+    }, [booking_detail]);
 
     return(
         <>
