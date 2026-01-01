9bbb1661c495f263a457fea22c5c921fc2247f98
HanThamarat <98406282+HanThamarat@users.noreply.github.com>
2026-01-02 05:33:35 +0700

    TA-9 fix: change icon

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -25,9 +25,9 @@ export const metadata: Metadata = {
   title: "The AJ Explorer",
   description: "The AJ Explorer",
   icons: {
-    icon: "/favicon.ico",         
-    shortcut: "/favicon.ico",
-    apple: "/favicon.ico",
+    icon: "/icon.png",         
+    shortcut: "/icon.png",
+    apple: "/icon.png",
   },
 };
 
